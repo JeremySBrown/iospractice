8fe1c03f00fdac98a7f2fdc6f588e356d147a8c4
Jeremy Brown <jeremy@jeremysbrown.com>
2014-03-22 10:17:33 -0400

    Initial Commit

---
+++ ControllerPractice/JSBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSBAppDelegate.h
+//  ControllerPractice
+//
+//  Created by Jeremy Brown on 3/22/14.
+//  Copyright (c) 2014 Jeremy S Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
